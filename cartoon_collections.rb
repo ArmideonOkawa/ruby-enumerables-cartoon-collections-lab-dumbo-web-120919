60817df6d2c995265ffb9c5b9dd24aff010fbda1
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-04 11:18:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)
   answer = false 
-  while i < planeteer_calls.length do
-    return true if planeteer_calls[i].length > 4
-  end
-  i += 1 
-end
-  false 
+  long_planeteer_calls.
 end
 
 def find_the_cheese# code an argument here
